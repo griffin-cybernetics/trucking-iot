62ce17a10b898ab891b930ae6508b2e6c76f9fde
Robert Griffin <rmgriffin@griffin-nc.com>
2018-02-01 13:03:15 -0500

    Updating Flow Template Path

--- scripts/nifi-flow-import.sh
+++ scripts/nifi-flow-import.sh
@@ -13,10 +13,10 @@ curl -u admin:admin -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"con
 # kafka-to-kafka.xml.gz
 # kafka-to-kafka-with-schema.xml.gz
 # kafka-to-kafka-with-schema-2.xml.gz
-scp $projDir/trucking-nifi-templates/flows/kafka-to-kafka-with-schema-2.xml.gz gcs-nifi-c02n01.local.griffin-nc.com:/tmp
-scp $projDir/trucking-nifi-templates/flows/kafka-to-kafka-with-schema-2.xml.gz gcs-nifi-c02n02.local.griffin-nc.com:/tmp
-scp $projDir/trucking-nifi-templates/flows/kafka-to-kafka-with-schema-2.xml.gz gcs-nifi-c02n03.local.griffin-nc.com:/tmp
-scp $projDir/trucking-nifi-templates/flows/kafka-to-kafka-with-schema-2.xml.gz gcs-nifi-c02n04.local.griffin-nc.com:/tmp
+scp $projDir/nifi-templates/flows/kafka-to-kafka-with-schema-2.xml.gz gcs-nifi-c02n01.local.griffin-nc.com:/tmp
+scp $projDir/nifi-templates/flows/kafka-to-kafka-with-schema-2.xml.gz gcs-nifi-c02n02.local.griffin-nc.com:/tmp
+scp $projDir/nifi-templates/flows/kafka-to-kafka-with-schema-2.xml.gz gcs-nifi-c02n03.local.griffin-nc.com:/tmp
+scp $projDir/nifi-templates/flows/kafka-to-kafka-with-schema-2.xml.gz gcs-nifi-c02n04.local.griffin-nc.com:/tmp
 
 ssh root@gcs-nifi-c02n01.local.griffin-nc.com << EOF
 
