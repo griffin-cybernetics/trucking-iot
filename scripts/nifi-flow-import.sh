bc9dd1cd998c3daefb47ef44c78637b4033526b7
Robert Griffin <rmgriffin@griffin-nc.com>
2018-02-01 12:57:05 -0500

    Nifi SCP Updates

--- scripts/nifi-flow-import.sh
+++ scripts/nifi-flow-import.sh
@@ -13,10 +13,42 @@ curl -u admin:admin -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"con
 # kafka-to-kafka.xml.gz
 # kafka-to-kafka-with-schema.xml.gz
 # kafka-to-kafka-with-schema-2.xml.gz
-scp -f $projDir/trucking-nifi-templates/flows/kafka-to-kafka-with-schema-2.xml.gz gcs-nifi-c02n01.local.griffin-nc.com:/tmp
+scp $projDir/trucking-nifi-templates/flows/kafka-to-kafka-with-schema-2.xml.gz gcs-nifi-c02n01.local.griffin-nc.com:/tmp
+scp $projDir/trucking-nifi-templates/flows/kafka-to-kafka-with-schema-2.xml.gz gcs-nifi-c02n02.local.griffin-nc.com:/tmp
+scp $projDir/trucking-nifi-templates/flows/kafka-to-kafka-with-schema-2.xml.gz gcs-nifi-c02n03.local.griffin-nc.com:/tmp
+scp $projDir/trucking-nifi-templates/flows/kafka-to-kafka-with-schema-2.xml.gz gcs-nifi-c02n04.local.griffin-nc.com:/tmp
+
 ssh root@gcs-nifi-c02n01.local.griffin-nc.com << EOF
 
 echo "Importing NiFi flow.  Existing flow backed up to flow.xml.gz.bak"
+rm -rf /var/lib/nifi/conf/flow.xml.gz.bak
+mv /var/lib/nifi/conf/flow.xml.gz /var/lib/nifi/conf/flow.xml.gz.bak
+cp -f /tmp/kafka-to-kafka-with-schema-2.xml.gz /var/lib/nifi/conf/flow.xml.gz
+
+EOF
+
+ssh root@gcs-nifi-c02n02.local.griffin-nc.com << EOF
+
+echo "Importing NiFi flow.  Existing flow backed up to flow.xml.gz.bak"
+rm -rf /var/lib/nifi/conf/flow.xml.gz.bak
+mv /var/lib/nifi/conf/flow.xml.gz /var/lib/nifi/conf/flow.xml.gz.bak
+cp -f /tmp/kafka-to-kafka-with-schema-2.xml.gz /var/lib/nifi/conf/flow.xml.gz
+
+EOF
+
+ssh root@gcs-nifi-c02n03.local.griffin-nc.com << EOF
+
+echo "Importing NiFi flow.  Existing flow backed up to flow.xml.gz.bak"
+rm -rf /var/lib/nifi/conf/flow.xml.gz.bak
+mv /var/lib/nifi/conf/flow.xml.gz /var/lib/nifi/conf/flow.xml.gz.bak
+cp -f /tmp/kafka-to-kafka-with-schema-2.xml.gz /var/lib/nifi/conf/flow.xml.gz
+
+EOF
+
+ssh root@gcs-nifi-c02n04.local.griffin-nc.com << EOF
+
+echo "Importing NiFi flow.  Existing flow backed up to flow.xml.gz.bak"
+rm -rf /var/lib/nifi/conf/flow.xml.gz.bak
 mv /var/lib/nifi/conf/flow.xml.gz /var/lib/nifi/conf/flow.xml.gz.bak
 cp -f /tmp/kafka-to-kafka-with-schema-2.xml.gz /var/lib/nifi/conf/flow.xml.gz
 
